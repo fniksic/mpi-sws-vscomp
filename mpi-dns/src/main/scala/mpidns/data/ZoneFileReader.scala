d0eb231b30fc674f6569e13f7a8de590d23df41a
Filip Niksic <fniksic@gmail.com>
2013-04-20 16:15:06 +0200

    Added mySplit to ZoneFileReader

--- mpi-dns/src/main/scala/mpidns/data/ZoneFileReader.scala
+++ mpi-dns/src/main/scala/mpidns/data/ZoneFileReader.scala
@@ -3,15 +3,36 @@ package mpidns.data
 import scala.io.Source
 
 object ZoneFileReader {
-  
+
   def read(path: String) = {
     try {
-    	for(line <- Source.fromFile(path).getLines()) {
-    		println("Parse line: " + line)
-    		    		
-    	}
+      for (line <- Source.fromFile(path).getLines()) {
+        println("Parse line: " + line)
+
+      }
     } catch {
-    	case ex: Exception => print(ex);
+      case ex: Exception => print(ex);
+    }
+  }
+
+  def firstWord(s: String): (String, Int) = s match {
+    case "" => ("", 0)
+    case t => {
+      val head = t.charAt(0)
+      if (head == ':') ("", 1)
+      else {
+        val (tmp, i) = firstWord(t.substring(1))
+        (head + tmp, i + 1)
+      }
+    }
+  }
+
+  def mySplit(s: String): ListString = s match {
+    case "" => Cons("", Nil())
+    case t => {
+      val (word, index) = firstWord(t)
+      val tail = t.substring(index)
+      Cons(word, mySplit(tail))
     }
   }
 
