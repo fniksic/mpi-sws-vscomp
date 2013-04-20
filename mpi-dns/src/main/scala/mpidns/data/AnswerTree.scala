71f3400665a23331583b8cd4b8bd85b864701a85
Johannes Kloos <jkloos@mpi-sws.org>
2013-04-21 00:54:15 +0200

    Added data structures.

--- mpi-dns/src/main/scala/mpidns/data/AnswerTree.scala
+++ mpi-dns/src/main/scala/mpidns/data/AnswerTree.scala
@@ -1,9 +1,21 @@
 package mpidns.data
 
-sealed abstract class RR(ttl: Integer) {}
+import java.net.Inet4Address
 
-sealed class AnswerTreeNode(children:Map[String, TreeNode], rrs: List[PlainRR]) {}
+sealed abstract class RR(ttl: Int) {}
+
+sealed case class RR_NS(ttl: Int, fqdn: Name, addrs: List[Inet4Address]) extends RR(ttl)
+sealed case class RR_A(ttl: Int, addr: Inet4Address) extends RR(ttl)
+sealed case class RR_SOA(ttl: Int, fqdn: Name, hostmaster: String,
+    refresh: Int, retry: Int, expire: Int, minimum: Int) extends RR(ttl)
+sealed case class RR_PTR(ttl: Int, fqdn: Name) extends RR(ttl)
+sealed case class RR_MX(ttl: Int, prio: Int, fqdn: Name) extends RR(ttl)
+sealed case class RR_TXT(ttl: Int, text: String) extends RR(ttl)
+sealed case class RR_CNAME(ttl: Int, fqdn: Name, child_records: List[RR]) extends RR(ttl)
+
+sealed class AnswerTreeNode(children:Map[String, TreeNode], rrs: List[RR],
+    authoritative: Boolean) {}
 
 sealed abstract class AnswerTree(root: AnswerTreeNode) {
-    
+    val node: AnswerTreeNode
 }
