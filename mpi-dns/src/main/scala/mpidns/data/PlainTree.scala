8cd74212823c29e74b5893740ed9f8638a413272
Max-Ferdinand Suffel <Max.Suffel@googlemail.com>
2013-04-20 23:55:04 +0200

    First zone file parser implementation

--- mpi-dns/src/main/scala/mpidns/data/PlainTree.scala
+++ mpi-dns/src/main/scala/mpidns/data/PlainTree.scala
@@ -1,13 +1,13 @@
 package mpidns.data
 
-import java.net.Inet4Address
+import java.net.InetAddress
 
 sealed abstract class PlainRR(ttl: Int)
 
 sealed case class PlainRR_NS(ttl: Int, fqdn: Name) extends PlainRR(ttl)
-sealed case class PlainRR_A(ttl: Int, addr: Inet4Address) extends PlainRR(ttl)
+sealed case class PlainRR_A(ttl: Int, addr: InetAddress) extends PlainRR(ttl)
 sealed case class PlainRR_SOA(ttl: Int, fqdn: Name, hostmaster: String,
-    refresh: Int, retry: Int, expire: Int, minimum: Int) extends PlainRR(ttl)
+    serial:Long, refresh: Int, retry: Int, expire: Int, minimum: Int) extends PlainRR(ttl)
 sealed case class PlainRR_PTR(ttl: Int, fqdn: Name) extends PlainRR(ttl)
 sealed case class PlainRR_MX(ttl: Int, prio: Int, fqdn: Name) extends PlainRR(ttl)
 sealed case class PlainRR_TXT(ttl: Int, text: String) extends PlainRR(ttl)
