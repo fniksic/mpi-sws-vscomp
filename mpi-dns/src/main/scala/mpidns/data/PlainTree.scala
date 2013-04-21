5481b6109a5c6b487a453e50f3e1b32eab34026e
Johannes Kloos <jkloos@mpi-sws.org>
2013-04-21 12:32:21 +0200

    Implemented PlainTree.
    Added implementation of PlainTree; now, we can actually build an
    instance.

--- mpi-dns/src/main/scala/mpidns/data/PlainTree.scala
+++ mpi-dns/src/main/scala/mpidns/data/PlainTree.scala
@@ -13,12 +13,46 @@ sealed case class PlainRR_MX(ttl: Int, prio: Int, fqdn: Name) extends PlainRR(tt
 sealed case class PlainRR_TXT(ttl: Int, text: String) extends PlainRR(ttl)
 sealed case class PlainRR_CNAME(ttl: Int, fqdn: Name) extends PlainRR(ttl)
 
-sealed class TreeNode(children:Map[String, TreeNode], rrs: List[PlainRR])
+sealed class TreeNode(Children:Map[String, TreeNode], Rrs: List[PlainRR]) {
+  val children = Children
+  val rrs = Rrs
+}
 
 sealed abstract class PlainTree {
   def addRR(name: Name, rr: PlainRR): PlainTree;  
 }
 
+object PlainTreeHelpers {
+  def build_path(fqdn: List[String], rr: PlainRR): TreeNode = {
+    var node = new TreeNode(Map(), List(rr))
+    for (np <- fqdn) {
+      node = new TreeNode(Map(np -> node), List())
+    }
+    return node
+  }
+}
+
+class NonemptyPlainTree(Root: TreeNode) extends PlainTree {
+  val root = Root
+  private def doAddRR(fqdn: List[String], node: TreeNode, rr: PlainRR): TreeNode = {
+    fqdn match {
+      case scala.collection.immutable.Nil => 
+        return new TreeNode(node.children, rr :: node.rrs)
+      case head :: tail =>
+        val new_child = node.children.get(head) match {
+          case None => PlainTreeHelpers.build_path(fqdn.reverse, rr)
+          case Some(child) => doAddRR(tail, child, rr)
+        }
+        return new TreeNode(node.children - head + (head -> new_child), node.rrs)
+    }
+  }
+  override def addRR(name: Name, rr: PlainRR): PlainTree = {
+    return new NonemptyPlainTree(doAddRR(name.fqdn.reverse, root, rr))
+  }
+}
+
 object EmptyPlainTree extends PlainTree {
-  override def addRR(name: Name, rr: PlainRR) = this;
+  override def addRR(name: Name, rr: PlainRR): PlainTree = {
+    return new NonemptyPlainTree(PlainTreeHelpers.build_path(name.fqdn, rr))
+  }
 }
